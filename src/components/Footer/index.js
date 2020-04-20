839f2157012a48a3cdc77ba08b92ae25a221276f
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-04-19 22:53:21 -0700

    removed social links, they look better higher on the page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,31 +5,7 @@ function Footer() {
   return (
     <footer className="footer">
       <span>
-        {' '}
         <h6>Rachel Rohrbach &copy; Copyright 2020</h6>
-        <div className="social-links">
-          <p className="li-link-social">
-            <a
-              className="social"
-              href="https://github.com/rachelrohrbach"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-github fa-3x"aria-hidden="true" style={{ color: "white" }}></i>
-            </a>
-          </p>
-          {' '}
-          <p className="li-link-social">
-            <a
-              className="social"
-              href="https://www.linkedin.com/in/rachelrohrbach/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-linkedin fa-3x" aria-hidden="true" style={{ color: "white" }}></i>
-            </a>
-          </p>
-        </div>
       </span>
     </footer>
   );
