1841dac84a063b63aa2d3c00f107b293781746cf
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-04-19 22:54:58 -0700

    changed background

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,34 +4,27 @@ import Row from '../components/Row';
 import Container from '../components/Container';
 import ProjectCard from '../components/ProjectCard';
 import projects from '../data/projects.json';
+import Hero from '../components/Hero';
 
 function Projects() {
   return (
-    <div
-      className="portfolio"
-      style={{
-        backgroundImage: `url(https://cdn.pixabay.com/photo/2017/09/06/13/18/background-2721423_960_720.jpg)`,
-        backgroundSize: 'auto',
-        backgroundPosition: 'center',
-        position: 'relative',
-      }}
-    >
-      <h1 style={{ textAlign: 'center', color: "white", paddingTop: "10px" }}>My Portfolio</h1>
+    <div>
+      <Hero />
       <Container>
         <Row>
-          <div className="col-">
-            {projects.map((project) => (
-              <div className="project-card" key={project.name}>
-                <ProjectCard
-                  name={project.name}
-                  description={project.description}
-                  imgUrl={project.imgUrl}
-                  github={project.github}
-                  link={project.link}
-                />
-              </div>
-            ))}
-          </div>
+          <Col size="md-12">
+              {projects.map((project) => (
+                <div className="project-card" key={project.name}>
+                  <ProjectCard
+                    name={project.name}
+                    description={project.description}
+                    imgUrl={project.imgUrl}
+                    github={project.github}
+                    link={project.link}
+                  />
+                </div>
+              ))}
+          </Col>
         </Row>
       </Container>
     </div>
