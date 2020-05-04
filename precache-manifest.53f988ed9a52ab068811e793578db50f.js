9e97fcbcd2757ecefd8da8c6f3dcf089b7020f57
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-05-03 21:19:06 -0700

    Updates

---
+++ precache-manifest.53f988ed9a52ab068811e793578db50f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1056bdc6c05adf6d1a6ee13f58622ab5",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "00a03d9c8986ea900d6e",
+    "url": "/react-portfolio/static/css/main.cdc8e73e.chunk.css"
+  },
+  {
+    "revision": "9498f5aeb21c6d112d3c",
+    "url": "/react-portfolio/static/js/2.15e01bf0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.15e01bf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a03d9c8986ea900d6e",
+    "url": "/react-portfolio/static/js/main.72ed7412.chunk.js"
+  },
+  {
+    "revision": "c01136dc6b7e393f307b",
+    "url": "/react-portfolio/static/js/runtime-main.d92c7dfa.js"
+  },
+  {
+    "revision": "17959a971882fb50f12adbf2c6632c09",
+    "url": "/react-portfolio/static/media/technologies.17959a97.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.6f1784618a1f57c8011f7e0865bbb6ad.js"
+  "/react-portfolio/precache-manifest.53f988ed9a52ab068811e793578db50f.js"
 );
 
 self.addEventListener('message', (event) => {
