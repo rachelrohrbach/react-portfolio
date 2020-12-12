01071aeac4733d4732622290a417cf8e07356544
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-12-11 21:43:15 -0800

    updated about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,7 +13,7 @@ function About() {
             <p className='text-center' style={{ fontSize: 24, color: 'white' }}>{}
               Web Developer with a passion for making responsive, user-friendly
               websites and a background in event management. Known for being
-              well-organized and collaborative with a strong work ethic.
+              well-organized and collaborative with a strong work ethic. Certificate in Full Stack Web Development from the University of Washington Coding Boot Camp with skills in HTML, CSS, JavaScript and React.  
                <br />
               
               <br />
